9e4d6a25b446d8e78bea575b948268c98170063c
Mike Chaliy <mike@chaliy.name>
2011-05-04 18:15:24 +0300

    Added README and help documentation

--- PsConfig/PsConfig.psm1
+++ PsConfig/PsConfig.psm1
@@ -9,8 +9,7 @@ Param(
     $Path = Resolve-Path $Path    
     if (Test-Path $Path){
         Write-Verbose "Read settings from $Path"
-        $settings = ConvertFrom-StringData ([IO.File]::ReadAllText($Path))
-        
+        $settings = ConvertFrom-StringData ([IO.File]::ReadAllText($Path))        
         if ($Encripted){    
             DecriptValue $settings[$Name]
         } else {
@@ -19,6 +18,43 @@ Param(
     } else {
         Write-Verbose "Path $Path was not found."
     }
+<#
+.Synopsis
+    Gets configuration setting from user folder or elsewhere.
+.Description 
+    The Get-Setting function gets value for given setting name. By default it uses home directory and file .Settings, but you can specify path to another file.
+    The Get-Setting uses INI-like format compatible with ConvertFrom-StringData, however later possible that this will change.
+    Additionally the Get-Setting function supports decripting values (encription/decription is provided by Data Protection API (DPAPI) and by default uses UserScope).
+    
+    NOTE: If you are using Get-Setting and Set-Setting with encription, be ware that values encripted by Data Protection API (DPAPI) could be decripted by another program that runs under user's scope.    
+.Parameter Name
+    Name of the setting to get. Try to use ASCII char set and refuse special chars. This function does not provide any fool proof.
+    
+.Parameter Encripted
+    Indicates that value will be decripted with Data Protection API (DPAPI) after retrieve.
+    
+.Parameter Path
+    Optional parameter, specifyies path to file with settings.    
+.Link
+    https://github.com/chaliy/psconfig
+.Link
+    Set-Setting
+    
+.Example
+    Get-Setting User
+
+    Description
+    -----------
+    Retrieves value from default settings file under the name User. Default settings file is $home\.Settings
+    
+.Example
+    Get-Setting User -Encripted
+
+    Description
+    -----------
+    Retrieves encripted with Data Protection API (DPAPI) value from default settings file under the name User
+    
+#>
 }
 
 function Set-Setting {
@@ -43,6 +79,46 @@ Param(
     }
     Write-Verbose "Write settings to $Path"
     Set-Content $Path (  ConvertToStringData $settings )
+<#
+.Synopsis
+    Sets configuration setting to user folder or elsewhere.
+.Description 
+    The Set-Setting function sets value for given setting name. By default it uses home directory and file .Settings, but you can specify path to another file.
+    The Set-Setting uses INI-like format compatible with ConvertFrom-StringData, however later possible that this will change.
+    Additionally the Set-Setting function supports encripting values (encription/decription is provided by Data Protection API (DPAPI) and by default uses UserScope).
+    
+    NOTE: If you are using Set-Setting and Set-Setting with encription, be ware that values encripted by Data Protection API (DPAPI) could be decripted by another program that runs under user's scope.    
+.Parameter Name
+    Name of the setting to store. Try to use ASCII char set and refuse special chars. This function does not provide any fool proof.
+    
+.Parameter Value
+    Value of the setting to store. Try to use ASCII char set and refuse special chars. This function does not provide any fool proof.
+    
+.Parameter Encripted
+    Indicates that value will be encripted with Data Protection API (DPAPI) before save.
+    
+.Parameter Path
+    Optional parameter, specifyies path to file with settings.    
+.Link
+    https://github.com/chaliy/psconfig
+.Link
+    Get-Setting
+    
+.Example
+    Set-Setting User user256
+
+    Description
+    -----------
+    Store value "user256" to default settings file under the name User. Default settings file is $home\.Settings
+    
+.Example
+    Set-Setting User user256 -Encripted
+
+    Description
+    -----------
+    Store encripted with Data Protection API (DPAPI) value "user256" to default settings file under the name User
+    
+#>
 }
 
 function ConvertToStringData($state){	
---
+++ Readme.md
@@ -0,0 +1,44 @@
+PsConfig Utils
+=============
+
+Set of commands to read/write simple settings.
+
+Features
+========
+
+1. Read/Write simple values under user's scope
+2. Protect presisted values with Data Protection API (DPAPI)
+
+Examples
+========
+
+Most simple example is
+
+    set-setting User user256 # Save setting
+    get-setting User # Retrieve setting
+    
+To protect data, use Encripted swtich
+
+    set-setting User user256 -Encripted # Save setting
+    get-setting User -Encripted # Retrieve setting
+
+Installation
+============
+
+With <a href="https://github.com/chaliy/psget">PsGet</a>, execute:
+
+    install-module PsConfig
+    
+And you are ready to go.
+   
+FAQ
+===
+
+Q: Is protected setting is really protected?
+A: PsConfig uses Data Protection API (DPAPI) under user scope to protect values. This means that value is protected from other people, but not form you or other program that runs on your behalf.
+
+
+Credits
+=======
+
+1. PsConfig uses PowerSpec to automate testing.  
