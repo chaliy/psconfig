82f8897fb7d48faaef6d13b26434bcd84baca1f8
Mike Chaliy <mike@chaliy.name>
2011-05-04 17:19:14 +0300

    Tests refactored to separate specs

--- Tests.ps1
+++ Tests.ps1
@@ -3,18 +3,33 @@ set-location $here
 import-module PowerSpec
 import-module .\PsConfig\PsConfig.psm1 -Force
 
-test-spec { { 1 + 2 } | should not throw }
+test-spec {
+    "When reading predefined setting by name"    
+    $result = get-setting User -Path .\TestData\Simple.Settings;
+    $result | should be_equal "user1"     
+}
 
-test-spec { 
-
-    "Get predefined setting by name"
+test-spec {     
+    "When setting value by name"
+    $Path = [IO.Path]::GetTempFileName()
+    set-setting User "user256" -Path $Path -Verbose
+    $result = get-setting User -Path $Path 
     
-    { get-setting User -Path .\TestData\Simple.Settings } | should be_equal "user1" 
+    $result | should be_equal "user256"
+}
 
-    "Set setting by name"
-    { 
-        $Path = [IO.Path]::GetTempFileName()
-        set-setting User "user256" -Path $Path -Verbose
-        get-setting User -Path $Path 
-    } | should be_equal "user256"
+test-spec {    
+    "When setting couple values by names"
+    
+    $Path = [IO.Path]::GetTempFileName()
+    set-setting User "user256" -Path $Path -Verbose
+    set-setting User2 "user257" -Path $Path -Verbose
+    set-setting User3 "user258" -Path $Path -Verbose    
+    $User = get-setting User -Path $Path;
+    $User2 = get-setting User2 -Path $Path;
+    $User3 = get-setting User3 -Path $Path
+        
+    $User | should be_equal user256
+    $User2 | should be_equal user257
+    $User3 | should be_equal user258
 }
